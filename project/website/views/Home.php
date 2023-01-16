f8400dc55ffd022b5b97ceff42d1bdebecf4d65c
LeviHentzepeter <55549014+LeviHentzepeter@users.noreply.github.com>
2023-01-16 18:06:49 +0100

    Interface
    Made interface for Overview And editing

--- project/website/views/Home.php
+++ project/website/views/Home.php
@@ -21,5 +21,32 @@
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+    <div class="container">
+			<div class="card mt-5">
+				<div class="card-header">
+				<h2>Edit this Student</h2>
+				</div>
+				<div class="card-body">
+				<form method="post">
+					<div class="form-group">
+						<label for="name">name</label>
+						<input value="<?php // $Student->name; ?>" name="name" type="text" id="name" class="form-control">
+					</div>
+					<div class="form-group">
+						<label for="desc">Class</label>
+						<input value="<?php // $Student->Class; ?>" name="desc" type="text" id="desc" class="form-control">
+					</div>
+					<div class="form-group">
+						<label for="articlecode">DathOfBirth</label>
+						<input value="<?php // $Student->DathOfBirth; ?>" name="articlecode" type="text" id="articlecode" class="form-control">
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-info">Edit this product</button>
+					</div>
+				</form>
+				</div>
+			</div>
+    </div>
+
 
 </html>
